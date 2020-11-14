b399e5e12184fb495900934d6134de8dc6a9584c
mpelszyk <marcin.pelszyk90@gmail.com>
2020-11-14 10:29:58 +0100

    Napisany program sprawdza poprawność wyboru computera z użytkownikiem: Program służy do granie w "zgadnij liczbę computera"

--- zagadka/Gra_w_zagadke.py
+++ zagadka/Gra_w_zagadke.py
@@ -1,2 +1,19 @@
-## Gra w zagadkę ###
-from
+from random import randint
+print("Guess the number from the range")
+comp = randint(1,100)
+user = -1
+i = 0
+while user != comp:
+    i += 1
+    try:
+        user = int(input("Guess the number: "))
+    except ValueError:
+        print("it' not a number!")
+        continue
+    if user < comp:
+        print("TO samll!")
+    elif user > comp:
+        print("To big!")
+    else:
+        print("You winn!")
+        break
